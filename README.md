9bc99bafc231f1e3fe385a00218e6dc541744986
Divyaamsh <divyaamsh1538.be20@chitkara.edu.in>
2022-11-16 15:01:54 +0530

    ST-4

---
+++ README.md
@@ -0,0 +1,5 @@
+![image](https://user-images.githubusercontent.com/78367762/195980623-6a4b765e-9e35-4fdc-935a-2512c7761825.png)
+
+![image](https://user-images.githubusercontent.com/78367762/195980630-8834bcba-6197-4bcd-84fe-60bbf86a03c7.png)
+
+![image](https://user-images.githubusercontent.com/78367762/195980641-54ff0f05-cab1-4303-a9e0-303e08c7fdc0.png)
---
+++ src/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className = 'aboutspace'>
+          <h1>PHONE NO - 946XXXXXXXX ----------------</h1>
+          <br/>
+          <h1>EMAIL ID - SA@gmail.com ---------------</h1>
+          <br/><br/>
+          <from className="fm">
+            <input type ='text' placeholder='enter your name'/>
+            <br/>
+            <input type ='email' placeholder='enter your mail'/>
+            <br/>
+            <input type ='text' placeholder='enter your country'/>
+            <br/>
+            <input type ='text' placeholder='enter some message'/>
+            <br/>
+            <button type='submit'>submit</button>
+          </from>
+          <br/>
+          <br/>
+          <h1>THANK U</h1>
+         
+        
+    </div>
+  )
+}
+
+export default Contact
